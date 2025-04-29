0cb003b849d87969a44c91cd0cacc705c628674e
619-technologies <619technologies@gmail.com>
2025-04-29 09:39:02 +0500

    Update page.tsx

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,11 +1,1 @@
-"use client";
-import React from "react";
-import { useContext } from "react";
-import { MainContext } from "../context/TestContext";
 
-function page() {
-  const { count } = useContext(MainContext);
-  return <div>{count}</div>;
-}
-
-export default page;
